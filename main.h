5059646c5f6c046b23970aa4499a0bd72bb6570d
amna0elamir <60971840+amna0elamir@users.noreply.github.com>
2023-10-17 00:58:46 +0200

    Update main.h

--- main.h
+++ main.h
@@ -3,3 +3,16 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
+#include <unistd.h>
+
+/**
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ */
+typedef struct _format
+{
+        char type;
+        int (*f)(va_list);
+} format;
