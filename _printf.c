86fe7e598482be55583cd8e7b4c3f43de23ff37c
Mygithubayaa <ayaalmouiz11@gmail.com>
2023-10-17 17:45:34 -0700

    update

---
+++ _printf.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _printf-print format
+ * @format:what we want toprint
+ * Return:intger
+*/
+int _printf(char *format, ...)
+{
+	va_list args;
+	va_start (args, format);
+	int count;
+	count = 0;
+
+	while (*format)
+	{
+	if (*format == '%')
+	format++;
+	if (*format == '%')
+	{
+	count += printing_percentage();
+	}
+	else if (*format == 'c')
+	{
+	count += printing_char(args);
+	}
+	else (*format == 's')
+	{
+	count += printing_string(args);
+	}
+	}
+	else 
+	count += putchar(*format);
+	format++;
+}
+va_end(args);
+return (count);
+}		
